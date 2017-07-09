52d2677b1d66896c2b96cba65f2ad4bbf8b39f1d
Shehan Atukorala <ultimateweapon432@gmail.com>
2017-07-09 10:39:56 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,78 +1,66 @@
-//RUN OUR JQUERY 
-var following = [];
 $(document).ready(function(){
+    //$("#fccStatus").html("HELLO WORLD");
+    var streamList = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas","shehan19","brunofin","asfsa"];
     
-    //FREE CODE CAMP STREAM INFO AND STATUS API CALL   
-    var url = "https://api.twitch.tv/kraken/streams/freecodecamp?client_id=ybwm93zqp4lt62qapw9t8pyd0t25p7";
-        $.getJSON(url, function(data1){
-            if (data1.stream===null){
-                $("#fccStatus").html("Free Code Camp is currently OFFLINE!");
-                 
-            }
-        else{
-                $("#fccStatus").html("Free Code Camp is currently ONLINE!");
-        }
-    }); 
    
-    var followerurl = "https://api.twitch.tv/kraken/users/freecodecamp/follows/channels?client_id=ybwm93zqp4lt62qapw9t8pyd0t25p7";
-    $.getJSON(followerurl, function(data2){ 
-        for (var i = 0; i < data2.follows.length; i++){
-            
-            var displayName = data2.follows[i].channel.display_name;
-            following.push(displayName);
-            
-        }
-        following.push("comster404");
-        following.push("brunofin");
-        following.push("ESL_SC2");
         
-        for (var i=0; i<following.length;i++){
-            var url2 = "https://api.twitch.tv/kraken/users/" + following[i] + "?client_id=ybwm93zqp4lt62qapw9t8pyd0t25p7"; //PROBLEM WITH THIS
+        $.getJSON("https://api.twitch.tv/kraken/streams/freecodecamp?client_id=ybwm93zqp4lt62qapw9t8pyd0t25p7").done(function(data){
+            $("#fccStatus").html("HELLO PROTEIN!!!");
+            if (data.stream === null){
                 
-             $.getJSON(url2).done(function(data3){
-                 
-               var logo;
-               var name;
-               var status; 
-               console.log(data3);
-               if (data3.error){
-                   logo = "https://s3-us-west-2.amazonaws.com/web-design-ext-production/p/Combologo_474x356.png"
-                   name = data3.message;
-                   status = data3.error;
-               }
-            });
-           /*$.getJSON(url2).fail(function(data4){
-                var logo = "https://s3-us-west-2.amazonaws.com/web-design-ext-production/p/Glitch_474x356.png";
-                var name = data4.message;   
-                var status = data4.error;
-                $("#followerInfo").prepend("<div class='row'>"+"<div class='col-md-4'>" + "<img src='"+ logo +"'>" + "</div>" + "<div class='col-md-4'>"+ name + "</div>" + "<div class='col-md-4'>" + status + "</div></div>")
-                
-           });*/
-                
-
-               
-               
-              //  }
-            
-
-             }
-        for (var i = 0;i<following.length;i++){
-            var onlineURL = "https://api.twitch.tv/kraken/streams/" + following[i];
-            $.getJSON(onlineURL, function(data4){
-                if(data4.status !=  null && data4.error != true){
-                    var logo = data4.stream.channel.logo;
-                    console.log(logo);
-                    var status;
-                    var name;
-                }
-            })
-        }
-             
-        
-    });
+                $("#fccStatus").html("FREE CODE CAMP IS OFFLINE");
+            }
+            else{
+                $("#fccStatus").html("FREE CODE CAMP IS ONLINE");
+            }
+        })
     
+    for (var i = 0; i < streamList.length ; i++){
+        $.ajax({
+            type:"GET",
+            url:"https://api.twitch.tv/kraken/channels/"+streamList[i],
+            headers:{
+                "Client-id":"ybwm93zqp4lt62qapw9t8pyd0t25p7",
+            },
+            success: function(data1){
+            
+                    $.getJSON("https://api.twitch.tv/kraken/streams/"+ data1.name + "?client_id=ybwm93zqp4lt62qapw9t8pyd0t25p7").done(function(data2){
+                        var name = data1._links.self.slice(38);
+                        
+                        var status = data1.stream;
+                        var logo = data1.logo;
+                        var link = data2._links.self;
+                        if (data2.stream === null){
+                        $("#offlineUsers").prepend("<a href='" + link + "?client_id=ybwm93zqp4lt62qapw9t8pyd0t25p7"+"'><div class='row'><div class='col-md-4'><img src='"+ logo +"'></div><div class='col-md-4'>'"+ name +"'</div><div class='col-md-4'>'"+ status + "'</div></div></a>");
+                }
+                else{
+                    var status = data1.stream;
+                        var logo = data1.logo;
+                        var link = "https://www.twitch.tv/" + data1.display_name;
+                        console.log(link);
+                    $("#onlineUsers").prepend("<a href='" + link +"' target=_blank><div class='row'><img src='" + logo + "'><div class='col-md-4'>'" + name + "'</div><div class='col-md-4'></div><div class='col-md-4'>'" + status + "'</div></div></a>");
+                }
+            });
+                //else{
+                //    $.getJSON("https://api.twitch.tv/kraken/streams/" + data1.name + "?client_id=ybwm93zqp4lt62qapw9t8pyd0t25p7").done(function(data2){
+                //        var name = data1._links.self.slice(37);
+                //        var status = data1.stream;
+                //        var logo = data1.logo;
+                //        var link = data2._links.self;
+                //        $("#onlineusers").prepend("<a href='" + link +"'><div class=row><div class='col-md-4'><img src='" + logo + "'></div><div class='col-md-4'>'"+ name +"'</div><div class='col-md-4'>'" + status + "'</div></div></a>");
+                //    });
+              //  }
+            },
+            error: function(data1){
+                
+                var errorName = data1.responseText;
+                alert(errorName);
+                var  errorLink = "No Link";
+                var errorLogo = "http://www.clker.com/cliparts/8/3/3/4/1195445190322000997molumen_red_round_error_warning_icon.svg.med.png";
+                var errorStatus = "Unprocessable Entity";
+                $("#offlineUsers").prepend("<a href='" + errorLink + "?client_id=ybwm93zqp4lt62qapw9t8pyd0t25p7"+"'><div class='row'><div class='col-md-4'><img src='"+ errorLogo +"'></div><div class='col-md-4'>'"+ errorName +"'</div><div class='col-md-4'>'"+ errorStatus + "'</div></div></a>");
+            }
+
+        });
+    }
 });
-    // front end skill : react 
-    // ITS ALL ABOUT THE SKILL SET AND EXPERIENCE
-    //Will free code camp be enough to get a front end job?
-    // it was for me, but you also have to learn from youtube and make projects
